2763ce6c85860810c2f58645392dee28c2f723a6
Alexander Skornyakov <a1212sk@gmail.com>
2021-07-09 22:16:44 +0300

    Storing data in memory

--- rest hello world/main.py
+++ rest hello world/main.py
@@ -4,16 +4,18 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+    "Alex": {"age": 29, "gender": "male"},
+    "Polina": {"age": 25, "gender": "female"}
+}
+
+
 class HelloWorld(Resource):
     def get(self, name):
-        return {"data":f'Hello {name}'}
+        return names[name]
 
-    def post(self):
-        return {"data": "Hello Worlds! POST"}
 
 api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
